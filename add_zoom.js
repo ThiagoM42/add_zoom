ade8b00ac5656af8c4d8679ee6c27269fa80c1ac
Thiago Matos Santos <tms.42@hotmail.com>
2022-01-28 11:44:10 -0300

    add subtree: true

--- add_zoom.js
+++ add_zoom.js
@@ -1,37 +1,49 @@
 class addParticipante{
 	constructor(tag_button){	
-		this.tag_button = tag_button		
-		this.div_pai= document.querySelector('.participants-section-container')			
+		this.tag_button = tag_button				
 		this.addZoom()
 		this.observer()
 		
 	}
 	addZoom(){
-	    this.admissao =  document.querySelectorAll(`${this.tag_button}`)
-		console.log("passou pela primeira parte do addZoom")
-		if(this.admissao){
-			const button_admitir = this.admissao[1]
-
-			if(button_admitir.textContent === "Admitir"){
-			   console.log("admitir if")
-				button_admitir.click()
+		try{
+			this.admissao =  document.querySelectorAll(`${this.tag_button}`)
+			this.div_pai= document.querySelector('.participants-section-container')	
+			if(!this.div_pai){
+				throw new Exception("##### Abrir a Aba de Participants  ########")
 			}
-			else{
-				let button_admitir_todos = Array.from(this.admissao).filter(d=>d.innerText === 'Admitir todos')[0]
-				if(button_admitir_todos){
-					button_admitir_todos.click()
-				}				
+			if(this.admissao){
+				const button_admitir = this.admissao[1]
+
+				if(button_admitir.textContent === "Admitir"){
+				   console.log("admitir if")
+					button_admitir.click()
+				}
+				else{
+					let button_admitir_todos = Array.from(this.admissao).filter(d=>d.innerText === 'Admitir todos')[0]
+					if(button_admitir_todos){
+						button_admitir_todos.click()
+					}else{
+						throw new Exception("##### Abrir a Aba de Participants  ########")
+					}				
+				}			
 			}			
+		}catch(error){
+			console.log("Habilitar a lista de participantes. 2")											
+			setTimeout(function(){new addParticipante('button')}, 6000)			
 		}
 	}
 
 	 observer(){
 		try{			
+			if(!this.div_pai){
+				throw new Exception("Aba Participants")
+			}
 			let observer = new MutationObserver(()=>this.addZoom())
 			//let observer = new MutationObserver(()=>console.log('teste'))
-			observer.observe(this.div_pai, {childList:true});			
+			observer.observe(this.div_pai, {childList:true, subtree:true});			
 		}catch(error){
-			console.log("Habilitar a lista de participantes.")											
+			console.log("Habilitar a lista de participantes. 3")											
 			setTimeout(function(){new addParticipante('button')}, 6000)
 		}
 	}
